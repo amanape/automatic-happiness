024c6add2b05d14141fa17a76ac9c7c7123756f3
amanape <stephanpsaras@gmail.com>
2022-10-20 18:26:47 +0200

    Clean project

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,10 @@
-import { describe, expect, it } from 'vitest';
+import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
   it('renders', () => {
-    const { getByText } = render(<App/>);
-    expect(getByText('count is 0')).toBeTruthy();
+    const { getByText } = render(<App />);
+    expect(getByText('Vite + React + TypeScript')).toBeTruthy();
   });
 });
