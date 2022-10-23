a18bce9b93be617e3a0e823b3ea0568cfbb8b893
amanape <stephanpsaras@gmail.com>
2022-10-23 18:21:44 +0200

    Refactor

--- src/components/PaymentForm.tsx
+++ src/components/PaymentForm.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {
   ErrorMessage, Field, Form, Formik,
 } from 'formik';
-import { PaymentFormValues } from '../types/paymentForm.interfaces';
+import { PaymentFormValues } from '../types/payment-form.interfaces';
 import { validateCreditCard, validateFullName, validateExpiryDate } from '../utils/form.validators';
 
 const PaymentForm: React.FC = () => {
@@ -21,60 +21,54 @@ const PaymentForm: React.FC = () => {
     const creditCardError = validateCreditCard(values.cardNumber);
     const expirationDateError = validateExpiryDate(values.expirationDate);
 
-    if (fullNameError) {
-      errors.fullName = fullNameError;
-    }
+    if (fullNameError) errors.fullName = fullNameError;
 
-    if (creditCardError) {
-      errors.cardNumber = creditCardError;
-    }
+    if (creditCardError) errors.cardNumber = creditCardError;
 
-    if (expirationDateError) {
-      errors.expirationDate = expirationDateError;
-    }
+    if (expirationDateError) errors.expirationDate = expirationDateError;
 
-    if (!values.cvv) {
-      errors.cvv = 'Required';
-    }
+    if (!values.cvv) errors.cvv = 'Required';
 
-    if (!values.zipCode) {
-      errors.zipCode = 'Required';
-    }
+    if (!values.zipCode) errors.zipCode = 'Required';
 
     return errors;
   };
 
   return (
-    <Formik
-      initialValues={initialValues}
-      validate={validate}
-      validateOnChange={false}
-      onSubmit={(values) => console.log(values)}
-    >
-      <Form>
-        <label htmlFor="fullName">Full Name</label>
-        <Field name="fullName" type="text" />
-        <ErrorMessage name="fullName" component="p" data-testid="full_name-error" />
+    <div>
+      <h1>Payment info</h1>
+      <Formik
+        initialValues={initialValues}
+        validate={validate}
+        validateOnChange={false}
+        onSubmit={(values) => console.log(values)}
+      >
+        <Form>
+          <label htmlFor="fullName">Full Name</label>
+          <Field name="fullName" type="text" placeholder="Your Name" />
+          <ErrorMessage name="fullName" component="p" data-testid="full_name-error" />
 
-        <label htmlFor="cardNumber">Card Number</label>
-        <Field name="cardNumber" type="text" />
-        <ErrorMessage name="cardNumber" component="p" data-testid="card_number-error" />
+          <label htmlFor="cardNumber">Card Number</label>
+          <Field name="cardNumber" type="text" placeholder="1234 1234 1234 1234" />
+          <ErrorMessage name="cardNumber" component="p" data-testid="card_number-error" />
 
-        <label htmlFor="expirationDate">Expiration</label>
-        <Field name="expirationDate" type="text" />
-        <ErrorMessage name="expirationDate" component="p" data-testid="expiration_date-error" />
+          <label htmlFor="expirationDate">Expiration</label>
+          <Field name="expirationDate" type="text" placeholder="MM/YY" />
+          <ErrorMessage name="expirationDate" component="p" data-testid="expiration_date-error" />
 
-        <label htmlFor="cvv">CVV</label>
-        <Field name="cvv" type="text" />
-        <ErrorMessage name="cvv" component="p" data-testid="cvv-error" />
+          <label htmlFor="cvv">CVV</label>
+          <Field name="cvv" type="text" placeholder="&#8226;&#8226;&#8226;" />
+          <ErrorMessage name="cvv" component="p" data-testid="cvv-error" />
 
-        <label htmlFor="zipCode">Zip Code</label>
-        <Field name="zipCode" type="text" />
-        <ErrorMessage name="zipCode" component="p" data-testid="zip_code-error" />
+          <label htmlFor="zipCode">Zip Code</label>
+          <Field name="zipCode" type="text" placeholder="Your Zip" />
+          <ErrorMessage name="zipCode" component="p" data-testid="zip_code-error" />
 
-        <button type="submit">Submit</button>
-      </Form>
-    </Formik>
+          <button type="submit">Confirm Payment</button>
+        </Form>
+      </Formik>
+      <p>You verify that this info is correct</p>
+    </div>
   );
 };
 
