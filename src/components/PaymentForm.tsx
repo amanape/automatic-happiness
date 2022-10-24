0ca5cb5ee3d429078e1925773c504e8f3a91bf29
amanape <stephanpsaras@gmail.com>
2022-10-24 12:26:20 +0200

    Minor styling

--- src/components/PaymentForm.tsx
+++ src/components/PaymentForm.tsx
@@ -4,6 +4,8 @@ import {
 } from 'formik';
 import { PaymentFormValues } from '../types/payment-form.interfaces';
 import { validateCreditCard, validateFullName, validateExpiryDate } from '../utils/form.validators';
+import styles from './PaymentForm.module.css';
+import MASCOT from '../assets/mascot.png';
 
 const PaymentForm: React.FC = () => {
   const initialValues: PaymentFormValues = {
@@ -35,39 +37,44 @@ const PaymentForm: React.FC = () => {
   };
 
   return (
-    <div>
-      <h1>Payment info</h1>
+    <div className={styles.payment_form}>
+      <div className={styles.header}>
+        <img src={MASCOT} alt="Payment mascot" />
+        <h3>Payment info</h3>
+      </div>
       <Formik
         initialValues={initialValues}
         validate={validate}
         validateOnChange={false}
         onSubmit={(values) => console.log(values)}
       >
-        <Form>
+        <Form className={styles.form}>
           <label htmlFor="fullName">Full Name</label>
           <Field name="fullName" type="text" placeholder="Your Name" />
           <ErrorMessage name="fullName" component="p" data-testid="full_name-error" />
 
           <label htmlFor="cardNumber">Card Number</label>
           <Field name="cardNumber" type="text" placeholder="1234 1234 1234 1234" />
-          <ErrorMessage name="cardNumber" component="p" data-testid="card_number-error" />
+          <ErrorMessage name="cardNumber" component="p" className={styles.error} data-testid="card_number-error" />
 
           <label htmlFor="expirationDate">Expiration</label>
           <Field name="expirationDate" type="text" placeholder="MM/YY" />
-          <ErrorMessage name="expirationDate" component="p" data-testid="expiration_date-error" />
+          <ErrorMessage name="expirationDate" component="p" className={styles.error} data-testid="expiration_date-error" />
 
           <label htmlFor="cvv">CVV</label>
           <Field name="cvv" type="text" placeholder="&#8226;&#8226;&#8226;" />
-          <ErrorMessage name="cvv" component="p" data-testid="cvv-error" />
+          <ErrorMessage name="cvv" component="p" className={styles.error} data-testid="cvv-error" />
 
           <label htmlFor="zipCode">Zip Code</label>
           <Field name="zipCode" type="text" placeholder="Your Zip" />
-          <ErrorMessage name="zipCode" component="p" data-testid="zip_code-error" />
+          <ErrorMessage name="zipCode" component="p" className={styles.error} data-testid="zip_code-error" />
 
-          <button type="submit">Confirm Payment</button>
+          <div className={styles.footer}>
+            <button type="submit">Confirm Payment</button>
+            <span>You verify that this info is correct</span>
+          </div>
         </Form>
       </Formik>
-      <p>You verify that this info is correct</p>
     </div>
   );
 };
